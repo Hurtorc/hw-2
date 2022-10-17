43ca72eb11356c8aa828cbd18a584756bfd43bf9
Hurtorc <anire88@gmail.com>
2022-10-17 15:55:42 -0700

    added random int function

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Password Characters
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "~", "`"]
 
 // Write password to the #password input
 function writePassword() {
@@ -19,8 +14,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Password Generator
 
+//Password Characters
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "~", "`"]
+
+function getRandomInt(max) {
+  return Math.ceil(Math.random() * max)
+}
+
+// Password Generator
 function generatePassword() {
   var passwordLength = prompt('How long would you like your password? (8-128)', '12')
   var useUppercase = confirm('Would you like to use uppercase leeters?')
